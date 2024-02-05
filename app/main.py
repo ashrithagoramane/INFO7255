d56b9c0f8cc92f996524504f324cf8565f67cae7
Ashritha Goramane <ashritha.goramane@gmail.com>
2024-02-05 10:08:02 -0500

    Added Flask boilerplate code

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+import logging
+
+load_dotenv()
+
+logging.basicConfig(filename='/var/log/webapp.log', level=logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.route('/health')
+def check_health():
+    """
+    Route to check health of the application
+    """
+    app.logger.info("Health request handled successfully")
+    return jsonify({'message': 'Application is healthy!'}), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
