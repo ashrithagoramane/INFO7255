0b6aa5d080cd1317bc65fbfd81bad9c9538a76f5
Ashritha Goramane <ashritha.goramane@gmail.com>
2024-02-06 12:24:59 -0500

    Added schema first draft

--- app/main.py
+++ app/main.py
@@ -1,20 +1,37 @@
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 import logging
+from flask_expects_json import expects_json
+import json
 
 load_dotenv()
 
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
+schema = json.load(open('./schemas/plan_schema.json'))
+
 app = Flask(__name__)
 
-@app.route('/health')
+@app.route('/health', methods=["GET"])
 def check_health():
     """
     Route to check health of the application
     """
     app.logger.info("Health request handled successfully")
     return jsonify({'message': 'Application is healthy!'}), 200
+
+@app.route('/plan', methods=['POST'])
+@expects_json(schema=schema)
+def create_plan():
+    """
+    Create a plan
+    """
+    try:
+        app.logger.info("Handling create plan request")
+    except Exception as e:
+        return {'message': 'Invalid request JSON. Failed to create user'}, 400
+    else:
+        return jsonify({'message': 'Plan created successfully!'}), 200
 
 
 if __name__ == '__main__':
