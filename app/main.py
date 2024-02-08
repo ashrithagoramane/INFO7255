6f25b5cbb936fe0b0529cff2c890739b54abb387
Ashritha Goramane <ashritha.goramane@gmail.com>
2024-02-08 13:02:19 -0500

    Added get and delete requests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from werkzeug.http import generate_etag
 from dotenv import load_dotenv
 import logging
 from flask_expects_json import expects_json
+import redis_util
 import json
 
 load_dotenv()
@@ -27,12 +29,51 @@ def create_plan():
     Create a plan
     """
     try:
+        request_data = request.get_json()
+        response_data = redis_util.create_plan(request_data)
+        etag = generate_etag(json.dumps(response_data).encode())
         app.logger.info("Handling create plan request")
     except Exception as e:
         return {'message': 'Invalid request JSON. Failed to create user'}, 400
     else:
-        return jsonify({'message': 'Plan created successfully!'}), 200
+        response = jsonify({'message': 'Plan created successfully!'})
+        response.set_etag(etag)
+        return response, 201
 
+@app.route('/plan', methods=['GET'], defaults={'plan_id': None})
+@app.route('/plan/<plan_id>', methods=['GET'])
+def get_plan_by_id(plan_id):
+    """
+    Get a plan
+    """
+    try:
+        plan_details = redis_util.get_plan(plan_id)
+        etag = generate_etag(json.dumps(plan_details).encode())
 
+        # Check if client's ETag matches with the current ETag
+        if request.headers.get('If-None-Match') == etag:
+            return '', 304
+
+        app.logger.info("Handling get plan request")
+    except Exception as e:
+        return {'message': f'Invalid request JSON. Failed to create user {str(e)}'}, 400
+    else:
+        response = jsonify(plan_details)
+        response.set_etag(etag)
+        return response, 201
+
+@app.route('/plan/<plan_id>', methods=['DELETE'])
+def delete_plan(plan_id):
+    """
+    Delete a plan
+    """
+    try:
+        redis_util.delete_plan(plan_id)
+        app.logger.info("Handling delete plan request")
+    except Exception as e:
+        return {'message': f'Invalid request JSON. Failed to create user {str(e)}'}, 400
+    else:
+        return {}, 204
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- app/redis_util.py
+++ app/redis_util.py
@@ -11,62 +11,16 @@ PORT = os.getenv("REDIS_SERVER_PORT", 6379)
 r = redis.Redis(host=HOST, port=PORT, decode_responses=True)
 
 def create_plan(json_data):
-    key = f'{json_data.get("objectType")}:{json_data.get("objectId")}'
+    key = f'{json_data.get("objectId")}'
     r.set(key, json.dumps(json_data))
+    return json_data
 
-# data = {
+def get_plan(plan_id=None):
+    if plan_id:
+        plan = json.loads(r.get(f'{plan_id}'))
+        return plan
+    plans = [json.loads(r.get(key)) for key in r.keys()]
+    return plans
 
-# 	"planCostShares": {
-# 		"deductible": 2000,
-# 		"_org": "example.com",
-# 		"copay": 23,
-# 		"objectId": "1234vxc2324sdf-501",
-# 		"objectType": "membercostshare"
-		
-# 	},
-# 	"linkedPlanServices": [{
-# 		"linkedService": {
-# 			"_org": "example.com",
-# 			"objectId": "1234520xvc30asdf-502",
-# 			"objectType": "service",
-# 			"name": "Yearly physical"
-# 		},
-# 		"planserviceCostShares": {
-# 			"deductible": 10,
-# 			"_org": "example.com",
-# 			"copay": 0,
-# 			"objectId": "1234512xvc1314asdfs-503",
-# 			"objectType": "membercostshare"
-# 		},
-# 		"_org": "example.com",
-# 		"objectId": "27283xvx9asdff-504",
-# 		"objectType": "planservice"
-# 	}, {
-# 		"linkedService": {
-# 			"_org": "example.com",
-# 			"objectId": "1234520xvc30sfs-505",
-# 			"objectType": "service",
-# 			"name": "well baby"
-# 		},
-# 		"planserviceCostShares": {
-# 			"deductible": 10,
-# 			"_org": "example.com",
-# 			"copay": 175,
-# 			"objectId": "1234512xvc1314sdfsd-506",
-# 			"objectType": "membercostshare"
-# 		},
-		
-# 		"_org": "example.com",
-		
-# 		"objectId": "27283xvx9sdf-507",
-# 		"objectType": "planservice"
-# 	}],
-
-
-# 	"_org": "example.com",
-# 	"objectId": "12xvxc345ssdsds-508",
-# 	"objectType": "plan",
-# 	"planType": "inNetwork",
-# 	"creationDate": "12-12-2017"
-# }
-# create_plan(data)
+def delete_plan(plan_id):
+    r.delete(f'{plan_id}')
